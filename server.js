3ae25f0c2e0b0c1dce716141fc8bff03cb3d47f0
RKeertish <131195123+RKeertish@users.noreply.github.com>
2023-10-08 18:07:36 +0530

    Added get request function.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
+const path = require('path')
 const port = 8081
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) =>{
+	res.sendFile('index.html')
+});
 
 app.listen(port, ()=> {
 	console.log("Listening on port " + port)
